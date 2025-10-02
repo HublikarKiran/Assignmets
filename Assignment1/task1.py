87b79be9c320c555c0497fbc10f0f6df04f2b470
connect.kiranhublikar@gmail.com <connect.kiranhublikar@gmail.com>
2025-10-02 17:10:13 +0530

    adding first assignment

---
+++ Assignment1/task1.py
@@ -0,0 +1,13 @@
+# Task 1: Perform Basic Mathematical Operations
+
+
+num_1 = int(input("Enter 1st number :"))
+num_2 = int(input("Enter 2nd number :"))
+print("Addition of two numbers is :", num_1+num_2)
+print("Subtraction of two numbers is :", num_1-num_2)
+print("Multiplication of two numbers is :", num_1*num_2)
+print("Division of two numbers is :", num_1/num_2, "Division writes quotient")
+print("Modulus of two numbers is :", num_1 % num_2, "Modulus writes reminder")
+print("Exponentiation of two numbers is :", num_1**num_2)
+print("Floor Division of two numbers is :", num_1//num_2,
+      "Floor division writes only integer part and leaves decimal part")
